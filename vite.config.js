960dfb07e66ae6fe5e540805c6b48fcd3454b795
BR-64 <smooot.stu@gmail.com>
2023-03-15 10:20:03 +0700

    css reset build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [
     laravel({
       input: [
+        "resources/css/reset.css",
         "resources/css/app.css",
         "resources/css/style3.css",
         // "resources/assets/scss/main.scss",
