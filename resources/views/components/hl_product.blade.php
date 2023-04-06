d552a221eed8bcf4ff1c41a27a7d3e6225ce46f7
BR-64 <smooot.stu@gmail.com>
2023-04-06 18:20:05 +0700

    fin recode first page

--- resources/views/components/hl_product.blade.php
+++ resources/views/components/hl_product.blade.php
@@ -1,16 +1,14 @@
 
-    <div class="mainHL">
+<div class="mainHL">
         <h4 class="deco">Highlight</h4>
         <div class="swiper mySwiper_hl">
             <div class="swiper-wrapper">
-                <div class="gridHLmain swiper-slide">
+                <div class="gridHLmain ">
                     @foreach($hlproducts as $key => $hlproduct)
                         @if($key<4)
-                    <div
-                    class="border border-1 border-gray-200 rounded-md hover:border-purple-600 transition-colors bg-white"
-                    >
+                    <div>
                         <a href="{{ route('product.view', $hlproduct->item_code) }}"
-                            class="aspect-w-3 aspect-h-2 block overflow-hidden">
+                            class=" block overflow-hidden">
                             <img
                             src="{{ $hlproduct->image }}"
                             alt="{{ $hlproduct->item_code }}"
@@ -18,53 +16,48 @@
                             />
                         </a>
                 </div>
-                @endif
-                @endforeach
+                        @endif
+                    @endforeach
             </div>
-
-            <div class="gridHLmain swiper-slide">
-                @foreach($hlproducts as $key => $hlproduct)
-                @if($key>=4 && $key <8)
-                    <div
-                    class="border border-1 border-gray-200 rounded-md hover:border-purple-600 transition-colors"
-                    >
+                <div class="gridHLmain ">
+                    @foreach($hlproducts as $key => $hlproduct)
+                        @if($key>=4 && $key <8)
+                    <div>
                         <a href="{{ route('product.view', $hlproduct->item_code) }}"
-                            class="aspect-w-3 aspect-h-2 block overflow-hidden">
+                            class=" block overflow-hidden">
                             <img
                             src="{{ $hlproduct->image }}"
                             alt="{{ $hlproduct->item_code }}"
                             class="HLpic"
                             />
                         </a>
-            </div>
-                    @endif
-                @endforeach
-        </div>
-
-        <div class="gridHLmain swiper-slide">
-            @foreach($hlproducts as $key => $hlproduct)
-            @if($key>=8 && $key <12)
-                <div
-                class="border border-1 border-gray-200 rounded-md hover:border-purple-600 transition-colors bg-white"
-                >
-                    <a href="{{ route('product.view', $hlproduct->item_code) }}"
-                        class="aspect-w-3 aspect-h-2 block overflow-hidden">
-                        <img
-                        src="{{ $hlproduct->image }}"
-                        alt="{{ $hlproduct->item_code }}"
-                        class="HLpic"
-                        />
-                    </a>
                 </div>
-                @endif
-            @endforeach
-    </div>
+                        @endif
+                    @endforeach
+            </div>
+                <div class="gridHLmain ">
+                    @foreach($hlproducts as $key => $hlproduct)
+                    @if($key>=8 && $key <12)
+                    <div>
+                        <a href="{{ route('product.view', $hlproduct->item_code) }}"
+                            class=" block overflow-hidden">
+                            <img
+                            src="{{ $hlproduct->image }}"
+                            alt="{{ $hlproduct->item_code }}"
+                            class="HLpic"
+                            />
+                        </a>
+                </div>
+                        @endif
+                    @endforeach
+            </div>
 
+            <div class="gridHLmain swiper-slide"></div>
+
+        <div class="gridHLmain swiper-slide"></div>
+        <div class="gridHLmain swiper-slide"></div>
     </div>
     <div class="swiper-pagination"></div>
-        </div>
-
-
 </div>
 
 <script>
@@ -73,4 +66,4 @@
         el: ".swiper-pagination",
       },
     });
-  </script>
+</script>
