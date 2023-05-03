ab3a979f460c836f2db4baaf395af30fd03217b6
BR-64 <smooot.stu@gmail.com>
2023-05-03 09:58:52 +0700

    edit csrf webhook

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,8 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/webhook/stripe',
         '/payment',
-        '/qr'
+        '/qr',
+        '/credit',
+        '/walletali'
     ];
 }
