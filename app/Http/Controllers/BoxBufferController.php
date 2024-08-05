9e19fb92544283a8a71664493fc640a4a4fce6f9
BR-64 <smooot.stu@gmail.com>
2024-08-05 10:05:21 +0700

    editboxbuffername

--- app/Http/Controllers/BoxBufferController.php
+++ app/Http/Controllers/BoxBufferController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BoxBufferController extends Controller
+class BoxbufferController extends Controller
 {
     //
 }
--- app/Models/boxbuffer.php
+++ app/Models/boxbuffer.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BoxBuffer extends Model
+class Boxbuffer extends Model
 {
     use HasFactory;
 
